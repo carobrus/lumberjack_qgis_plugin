855f527768b6d6794a6fa283b1e9aeb6f3f3e2bb
Carolina Bruscantini <bruscantinic@gmail.com>
2019-08-26 17:02:11 -0300

    Changes input to use a directory
    Input now should be a directory with the folders of the landsat images as they are downloaded.
    The input file defines the region to consider when performing the textures.
    As a result, an output files gets created in each image directory. This file's name is created with the common part of the string that define the landsat 8 images, plus a "text.tif" at the end. This files holds one band for each texture.

--- grass_script/r.out.haralickFeatures.py
+++ grass_script/r.out.haralickFeatures.py
@@ -7,8 +7,14 @@
 #% keyword: features
 #%end
 
-#%option G_OPT_F_BIN_INPUT
+#%option G_OPT_M_DIR
 #% key: input
+#% type: string
+#% required : yes
+#%end
+
+#%option G_OPT_F_BIN_INPUT
+#% key: input_file
 #% type: string
 #% required : yes
 #%end
@@ -47,11 +53,6 @@
 #% description: Rescales instead of recoding the image
 #%end
 
-#%option G_OPT_F_OUTPUT
-#% key: output
-#% type: string
-#% required : yes
-#%end
 
 import sys
 import grass.script as gscript
@@ -65,90 +66,103 @@ import atexit
 
 def main():
 	options,flags = gscript.parser()
-	inputFile = options['input']
+	input_dir = options['input']
+	input_file = options['input_file']
 	sizeMovingWindow = options['size']
 	distance = options['distance']
 	numCategories = options['categories']
-	output = options['output']
 	recode = flags['r']
 
 	start_time = time.time()
+
 	try:
-		## Obtain bandCount to process each raster band
-		bandCount = gdal.Open(inputFile).RasterCount
+		##Validate parameters
+		if (int(sizeMovingWindow) % 2 == 0) or (int(sizeMovingWindow) < 2) or (int(distance) >= int(sizeMovingWindow)) or (int(numCategories) > 255):
+			raise
 	except:
-		print "ERROR: Unable to open file: ", inputFile
-
+			print "Size of moving windows must be odd and >=3."
+			print "The distance must be smaller than the size of the moving window."
+			print "The raster map cannot have more than 255 categories."
 	else:
-		try:
-			##Validate parameters
-			if (int(sizeMovingWindow) % 2 == 0) or (int(sizeMovingWindow) < 2) or (int(distance) >= int(sizeMovingWindow)) or (int(numCategories) > 255):
-				raise
-		except:
-				print "Size of moving windows must be odd and >=3."
-				print "The distance must be smaller than the size of the moving window."
-				print "The raster map cannot have more than 255 categories."
-		else:
-			gscript.run_command('g.mapset', mapset='PERMANENT')
+		gscript.run_command('g.mapset', mapset='PERMANENT')
+		gscript.run_command('g.proj', flags='c', georef=input_file)
+		gscript.run_command('r.in.gdal', flags='k', input=input_file, output='region_raster', overwrite=True)
+		## Manages the boundary definitions for the geographic region.
+		gscript.run_command('g.region', raster='region_raster')
 
-			gscript.run_command('g.proj', flags='c', georef=inputFile)
+		# r=root, d=directories, f = files
+		for r, d, f in os.walk(input_dir):
+			print "Checking directory: ", r
+			count = 1
+			for file in f:
+				full_path = os.path.join(r, file)
+				if (full_path[-9:-5] == "band"):
+					print "File: ", full_path
+					gscript.run_command('r.in.gdal', flags='k', input=full_path, output='inputBands.{}'.format(count), overwrite=True)
+					if (count == 1):
+						output_file = full_path[:-12] + "text.tif"
+						print "Output File to be created: ", output_file
+					count += 1
 
-			## Imports raster data into a GRASS raster map using GDAL library.
-			gscript.run_command('r.in.gdal', flags='k', input=inputFile, output='inputBands', overwrite=True)
+			if (count == 8):
 
-			## Manages the boundary definitions for the geographic region.
-			gscript.run_command('g.region', raster='inputBands.1')
+				bandCount = 7
+				rasterName = []
 
-			print "Band count: ", bandCount
+				inputRescale  = "inputBands.{}"
+				outputRescale = "bandsRescale.{}"
 
-			rasterName = []
-			filename, file_extension = os.path.splitext(inputFile)
+				if recode == False:
+					for band in range(1,bandCount+1):
+						rules = gscript.read_command('r.quantile', flags='r', input=inputRescale.format(band), quantiles=numCategories, overwrite=True, quiet=True)
+						print "Recoding raster map {}".format(band)
+						gscript.write_command('r.recode', input=inputRescale.format(band), output=outputRescale.format(band), rules='-', overwrite=True, stdin=rules)
+				else:
+					for band in range(1,bandCount+1):
+						gscript.run_command('r.rescale', input=inputRescale.format(band), output=outputRescale.format(band), to='1,'+numCategories, overwrite=True)
 
-			inputRescale  = "inputBands.{}"
-			outputRescale = "bandsRescale.{}"
+				outputTexture = "band.{}"
 
-			if recode == False:
+				queue = ParallelModuleQueue(nprocs=4)
+				texture = Module('r.texture', flags='n', method=['asm','contrast','var','idm','entr'], size=sizeMovingWindow, distance=distance, overwrite=True, run_=False)
+
 				for band in range(1,bandCount+1):
-					rules = gscript.read_command('r.quantile', flags='r', input=inputRescale.format(band), quantiles=numCategories, overwrite=True, quiet=True)
-					print "Recoding raster map {}".format(band)
-					gscript.write_command('r.recode', input=inputRescale.format(band), output=outputRescale.format(band), rules='-', overwrite=True, stdin=rules)
-			else:
+					m = deepcopy(texture)(input=outputRescale.format(band), output=outputTexture.format(band))
+					queue.put(m)
+				queue.wait()
+
 				for band in range(1,bandCount+1):
-					gscript.run_command('r.rescale', input=inputRescale.format(band), output=outputRescale.format(band), to='1,'+numCategories, overwrite=True)
+					rasterName.extend([outputTexture.format(band)+'_ASM', outputTexture.format(band)+'_Contr',outputTexture.format(band)+'_Var',outputTexture.format(band)+'_IDM',outputTexture.format(band)+'_Entr'])
+					## Generate group to export the tiff image
+					groupInput = outputTexture.format(band)+'_ASM,'+outputTexture.format(band)+'_Contr,'+outputTexture.format(band)+'_Var,'+outputTexture.format(band)+'_IDM,'+outputTexture.format(band)+'_Entr'
+					gscript.run_command('i.group', group='outFileBands', input=groupInput, quiet=True)
 
-			outputTexture = "band.{}"
+				## Creating tiff image
+				gscript.run_command('r.out.gdal', flags='cm', input='outFileBands', output=output_file, format='GTiff', type='Float64', overwrite=True, verbose=True)
 
-			queue = ParallelModuleQueue(nprocs=4)
-			texture = Module('r.texture', flags='n', method=['asm','contrast','var','idm','entr'], size=sizeMovingWindow, distance=distance, overwrite=True, run_=False)
+				print "Finished creating features"
+				print "Renaming raster bands"
 
-			for band in range(1,bandCount+1):
-				m = deepcopy(texture)(input=outputRescale.format(band), output=outputTexture.format(band))
-				queue.put(m)
-			queue.wait()
+				src_ds = gdal.Open(output_file)
+				for band in range(src_ds.RasterCount):
+					band += 1
+					src_ds.GetRasterBand(band).SetDescription(rasterName[band-1])
 
-			for band in range(1,bandCount+1):
-				rasterName.extend([outputTexture.format(band)+'_ASM', outputTexture.format(band)+'_Contr',outputTexture.format(band)+'_Var',outputTexture.format(band)+'_IDM',outputTexture.format(band)+'_Entr'])
-				## Generate group to export the tiff image
-				groupInput = outputTexture.format(band)+'_ASM,'+outputTexture.format(band)+'_Contr,'+outputTexture.format(band)+'_Var,'+outputTexture.format(band)+'_IDM,'+outputTexture.format(band)+'_Entr'
-				gscript.run_command('i.group', group='outFileBands', input=groupInput, quiet=True)
-
-			## Creating tiff image
-			gscript.run_command('r.out.gdal', flags='cm', input='outFileBands', output=output, format='GTiff', type='Float64', overwrite=True, verbose=True)
-
-			print "Finished creating features"
-			print "Renaming raster bands"
-
-			src_ds = gdal.Open(output)
-			for band in range(src_ds.RasterCount):
-				band += 1
-				src_ds.GetRasterBand(band).SetDescription(rasterName[band-1])
-
-			elapsed_time = time.time() - start_time
-			print "Finished in: ", elapsed_time, " seconds"
+		elapsed_time = time.time() - start_time
+		print "Finished in: ", elapsed_time, " seconds"
 
 
 def cleanup():
 	## Remove raster maps from group
+
+	cf = gscript.find_file(name='region_raster', element='group')
+	if not cf['fullname'] == '':
+		gscript.run_command('g.remove', flags='f', type='group', name='region_raster', quiet=True)
+
+
+	cf = gscript.find_file(name='inputBands', element='group')
+	if not cf['fullname'] == '':
+		gscript.run_command('g.remove', flags='f', type='group', name='inputBands', quiet=True)
 
 	cf = gscript.find_file(name='outFileBands', element='group')
 	if not cf['fullname'] == '':
