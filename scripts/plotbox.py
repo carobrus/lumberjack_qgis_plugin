c0ec8db071ce69ab4187af39a6d176a6f8d3d278
Carolina Bruscantini <bruscantinic@gmail.com>
2019-10-14 20:18:01 -0300

    Visual changes to the boxplot

--- scripts/plotbox.py
+++ scripts/plotbox.py
@@ -10,19 +10,22 @@ class PlotWindow(QMainWindow):
         super(PlotWindow, self).__init__(parent)
 
         self.title = 'Thresholds'
-        self.width = 700
-        self.height = 480
+        self.width = 1000
+        self.height = 700
         self.data = data
         self.days = days
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
-        # self.resize(self.width, self.height)
-        self.setFixedSize(self.width, self.height)
+        self.resize(self.width, self.height)
+        # self.setFixedSize(self.width, self.height)
 
         m = PlotCanvas(self, data=self.data, days=self.days)
         m.move(0,0)
+
+        layout = QVBoxLayout()
+        layout.addWidget(m)
 
         self.show()
 
@@ -32,6 +35,7 @@ class PlotCanvas(FigureCanvas):
     def __init__(self, parent=None, dpi=100, data=None, days=None):
         fig = Figure(dpi=dpi)
         fig.tight_layout()
+        fig.set_size_inches(10,7,forward=True)
         self.axes = fig.add_subplot(111)
         self.data = data
         self.days = days
@@ -47,5 +51,6 @@ class PlotCanvas(FigureCanvas):
         ax = self.figure.add_subplot(111)
         ax.set_xticklabels(self.days, rotation=90)
         ax.boxplot(data, showfliers=False, positions=self.days, widths=3)
+        ax.set_xlim(xmin=0, xmax=366)
         # ax.set_title('Threshholds')
         self.draw()
