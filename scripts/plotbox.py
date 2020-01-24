d561cbdc2210f0c8e0e7dce8e30ceae0b0ebd724
Carolina Bruscantini <bruscantinic@gmail.com>
2020-01-24 16:00:51 -0300

    Boxplot resizable

--- scripts/plotbox.py
+++ scripts/plotbox.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout
 from PyQt5.QtWidgets import QSizePolicy, QMessageBox, QWidget
+from PyQt5 import QtGui
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas)
 from matplotlib.figure import Figure
@@ -11,7 +12,7 @@ class PlotWindow(QMainWindow):
     def __init__(self, parent=None, data=None, days=None):
         super(PlotWindow, self).__init__(parent)
         self.title = 'Thresholds'
-        self.width = 1000
+        self.width = 800
         self.height = 700
         self.data = data
         self.days = days
@@ -20,38 +21,37 @@ class PlotWindow(QMainWindow):
 
     def initUI(self):
         self.setWindowTitle(self.title)
+        self.setMinimumSize(500, 500)
         self.resize(self.width, self.height)
-        # self.setFixedSize(self.width, self.height)
-        m = PlotCanvas(self, data=self.data, days=self.days)
-        m.move(0,0)
-        layout = QVBoxLayout()
-        layout.addWidget(m)
+
+        layout = QtGui.QVBoxLayout()
+        self.plot_canvas = PlotCanvas(self, data=self.data, days=self.days)
+        layout.addWidget(self.plot_canvas)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
         self.show()
 
 
 class PlotCanvas(FigureCanvas):
     def __init__(self, parent=None, dpi=100, data=None, days=None):
-        fig = Figure(dpi=dpi)
-        fig.tight_layout()
-        fig.set_size_inches(10,7,forward=True)
-        self.axes = fig.add_subplot(111)
+        self.fig = Figure(dpi=dpi)
+        self.axes = self.fig.add_subplot(111)
+        FigureCanvas.__init__(self, self.fig)
+        self.setParent(parent)
+        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.data = data
         self.days = days
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-
-        FigureCanvas.setSizePolicy(
-            self, QSizePolicy.Expanding, QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
+        # FigureCanvas.updateGeometry(self)
         self.plot()
+        self.fig.tight_layout()
 
 
     def plot(self):
         data = self.data
-        ax = self.figure.add_subplot(111)
         if (self.days != []):
-            ax.set_xticklabels(self.days, rotation=90)
-            ax.boxplot(data, showfliers=False, positions=self.days, widths=3)
-            ax.set_xlim(xmin=0, xmax=366)
-        # ax.set_title('Threshholds')
+            self.axes.set_xticklabels(self.days, rotation=90)
+            self.axes.boxplot(data, showfliers=False, positions=self.days, widths=3)
+            self.axes.set_xlim(xmin=0, xmax=366)
+        self.axes.set_title('Threshholds')
         self.draw()
